caad42164c9477badefc88a2dc29688503e2a498
glorianaz <150281309+glorianaz@users.noreply.github.com>
2023-12-11 13:30:36 -0500

    Title Screen

--- TitleScreen.java
+++ TitleScreen.java
@@ -8,7 +8,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class TitleScreen extends World
 {
-
+    Label titleLabel = new Label("Watermelon", 55);
+    
     /**
      * Constructor for objects of class TitleScreen.
      * 
@@ -17,5 +18,18 @@ public class TitleScreen extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+        
+        addObject(titleLabel, getWidth()/2, getHeight()/2 - 60);
+        prepare();
+    }
+    
+    private void prepare()
+    {
+        Label label1 = new Label("Press Space to Play!", 35);
+        addObject(label1,getWidth()/2, getHeight()/2 - 15);
+        Label label2 = new Label("Combine fruits until it evolves to a", 30); 
+        Label label3 = new Label("WATERMELON", 30);
+        addObject(label2, getWidth()/2, getHeight()/2 + 20);
+        addObject(label3, getWidth()/2, getHeight()/2 + 47);
     }
 }
